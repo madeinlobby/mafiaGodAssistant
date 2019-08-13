243c814be8eb86a55d91a6024a06dd98c8548d92
zahrayousefijamarani <z.y.j.1379@gmail.com>
2019-08-13 22:32:54 +0430

    user validations

--- MGA/models.py
+++ MGA/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=16)
+    bio = models.CharField(max_length=250)
+    phoneNumber = models.BigIntegerField()
+    city = models.CharField(max_length=200)
+    email = models.EmailField()
--- MGA/serializers.py
+++ MGA/serializers.py
@@ -1,2 +1,42 @@
+import re
+
 from rest_framework import serializers
 
+from MGA.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User,
+        fields = ['username', 'name', 'email', 'city', 'bio', 'phoneNumber', 'password']
+
+    def validate_username(self, value):
+        qs = User.objects.filter(username__exact=value)
+        if self.instance:
+            qs = qs.exclude(id=self.instance.id)
+        if qs.exists():
+            raise serializers.ValidationError("Username should be unique")
+        return value
+
+    def validate_phoneNumber(self, value):
+        if len(value) != 11:
+            raise serializers.ValidationError("Your phone number should have 11 digits")
+        rule = re.compile(r'/^09[0-9]{9}$/')
+        if not rule.search(value):
+            raise serializers.ValidationError("Your phone number is incorrect")
+        return value
+
+    def validate_bio(self, value):
+        if len(value) > 249:
+            raise serializers.ValidationError("Your bio should have at least 250 characters")
+
+    def validate_password(self, value):
+        if len(value) < 5:
+            raise serializers.ValidationError("Your password is too short! It must be between 5 and 15 characters")
+        if len(value) > 15:
+            raise serializers.ValidationError("Your password is too long! It must be between 5 and 15 characters")
+        if not re.search(r'[A-Z]|[a-z]', value) & re.search("[!@#$%^&*]", value):
+            raise serializers.ValidationError("Weak password!")
+        if not re.search("[0-9]", value) & re.search("[!@#$%^&*]", value):
+            raise serializers.ValidationError("Weak password!")
+        return value
