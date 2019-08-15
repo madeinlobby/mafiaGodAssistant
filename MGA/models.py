25e87a6e12ad1b014d68136aa298ec41fec1050c
savaw <savaw97@gmail.com>
2019-08-15 12:14:29 +0430

    added event & org

--- MGA/models.py
+++ MGA/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 
 class User(models.Model):
@@ -10,3 +12,20 @@ class User(models.Model):
     city = models.CharField(max_length=200)
     email = models.EmailField()
     confirm = models.BooleanField(default=False)
+
+
+class Event(models.Model):
+    # location todo
+    date = models.DateTimeField(default=timezone.now)
+    capacity = models.IntegerField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # event ?
+    # members todo
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=300)
+
+
+class Organization(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # admins todo
+
