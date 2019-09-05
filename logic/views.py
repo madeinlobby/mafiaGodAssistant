e8a647f8d253e29220d4cd55dec81e45d0380315
elie <masoumi.elnaz2@gmail.com>
2019-09-05 18:46:16 +0430

    create game added

--- logic/views.py
+++ logic/views.py
@@ -3,8 +3,19 @@ from logic.models import Game
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from MGA.models import Event
+from logic.serializers import GameSerializer
 
 
+
+@api_view(['GET','POST'])
+def create_game(request):
+    event_id = request.data.get('event id')
+    event = Event.objects.get(id=event_id)
+    game = Game.objects.all().get()
+    Game.objects.create(owner=event.owner,event=event)
+
+    serializer = GameSerializer(game)
+    return Response(serializer.data)
 
 
 
