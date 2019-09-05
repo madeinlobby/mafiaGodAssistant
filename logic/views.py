0058624ff046e6757680272ee0ad4ee8f79a190f
elie <masoumi.elnaz2@gmail.com>
2019-09-05 18:16:42 +0430

    logic serializer added

--- logic/views.py
+++ logic/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from logic.models import Game
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from MGA.models import Event
 
-# Create your views here.
+
+
+
+
